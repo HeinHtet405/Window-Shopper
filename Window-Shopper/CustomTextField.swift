d30f19409350a3df899c318115b40284333d6051
Hein Htet <heinhtet@Heins-iMac.local>
2017-09-01 16:52:51 +0630

    Complete Window-Shopper Lesson 2

---
+++ Window-Shopper/CustomTextField.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTextField.swift
+//  Window-Shopper
+//
+//  Created by Hein Htet on 9/1/17.
+//  Copyright © 2017 Hein Htet. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [NSForegroundColorAttributeName: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
--- Window-Shopper/ViewController.swift
+++ Window-Shopper/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencyTextField: CustomTextField!
+    
+    @IBOutlet weak var priceTextField: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+
+        calBtn.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        calBtn.setTitle("Calculate", for: .normal)
+        calBtn.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        calBtn.addTarget(self, action: #selector(ViewController.calculate), for: .touchUpInside)
+        
+        currencyTextField.inputAccessoryView = calBtn
+        priceTextField.inputAccessoryView = calBtn
+    }
+    
+    func calculate() {
+        print("We got here")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
